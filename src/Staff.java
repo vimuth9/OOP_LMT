82128733f53be025fdfd59b146d274c7a3b95c8c
vimuth9 <vimuthwic3@gmail.com>
2025-01-10 13:45:00 +0530

    Staff.java class changes of method name

--- src/Staff.java
+++ src/Staff.java
@@ -24,7 +24,7 @@ public class Staff {
         return this.password.equals(inputPassword);
     }
 
-    public String toString() {
+    public String displayStaffDetails() {
         return staffId + ": " + username;
     }
 }
